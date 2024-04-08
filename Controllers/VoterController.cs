fd3291820e900e34cd0683d608812134267b19c4
amitkhare <amit.khare@nagarro.com>
2024-04-09 00:04:03 +0530

    Add project files.

---
+++ Controllers/VoterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VoterManagement.api.Model;
+
+namespace VoterManagement.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoterController : ControllerBase
+    {
+        public static List<Voters> voters = new List<Voters> ()
+        {
+            new Voters()
+            {
+                Id = 1,Name="Vikram",Ward="South", Age=23
+            },
+            new Voters()
+            {
+                Id = 2,Name="Namit",Ward="West", Age=43
+            },
+            new Voters()
+            {
+                Id = 3,Name="Akram",Ward="North", Age=34
+            },
+            new Voters()
+            {
+                Id = 4,Name="Snehil",Ward="East", Age=54
+            },
+
+        };
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok (voters);
+
+        }
+        [HttpPost]
+        public IActionResult Post(Voters voter)
+        {
+            voters.Add(voter);
+            return Ok (voters);
+        }
+        
+    }
+}
---
+++ Model/Voters.cs
@@ -0,0 +1,11 @@
+﻿namespace VoterManagement.api.Model
+{
+    public class Voters
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ward { get; set; }
+        public int Age { get; set; }
+
+    }
+}
